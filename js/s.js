4bbb66dbeaea4645c8ffbaa5bd2491369d6dc0bb
GemmaFerrari25 <doitrung9810@gmail.com>
2024-09-15 10:39:42 +0700

    Update s.js

--- js/s.js
+++ js/s.js
@@ -1,15 +1,41 @@
-(function() {
-    // Find the container where the ad should be displayed
-    var adContainer = document.getElementById('ad-container');
+// Create the ad container dynamically
+var ad = document.createElement('div');
 
-    // Create an ad element (could be an image, iframe, etc.)
-    var ad = document.createElement('div');
-    ad.innerHTML = `
-        <a href="https://www.quora.com/profile/Gemma-Ferrari-1" target="_blank">
-            <img src="https://qph.cf2.quoracdn.net/main-thumb-2723536298-200-kkndpeaaeaqdxivqemcsnbnusfejisgf.jpeg" alt="Ad Banner" style="width:300px; height:250px;">
-        </a>
+// List of ads with URL, image source, and title
+var ads = [
+    {
+        url: 'https://www.quora.com/profile/Gemma-Ferrari-1m',
+        img: 'https://qph.cf2.quoracdn.net/main-thumb-2723536298-200-kkndpeaaeaqdxivqemcsnbnusfejisgf.jpeg',
+        title: 'Ad Title 1'
+    },
+    {
+        url: 'https://example-ad2.com',
+        img: 'https://example.com/ad-thumb2.jpg',
+        title: 'Ad Title 2'
+    },
+    {
+        url: 'https://example-ad3.com',
+        img: 'https://example.com/ad-thumb3.jpg',
+        title: 'Ad Title 3'
+    }
+];
+
+// Generate the HTML structure for the ads
+var adHTML = '<div style="display: flex; flex-direction: column;">'; // Wrapper div
+ads.forEach(function(item) {
+    adHTML += `
+        <div style="display: flex; align-items: center; margin-bottom: 10px;">
+            <a href="${item.url}" target="_blank" style="text-decoration: none; color: inherit; display: flex; align-items: center;">
+                <img src="${item.img}" alt="${item.title}" style="width: 120px; height: auto; margin-right: 10px;">
+                <span>${item.title}</span>
+            </a>
+        </div>
     `;
+});
+adHTML += '</div>';
 
-    // Append the ad element to the container
-    adContainer.appendChild(ad);
-})();
+// Insert the generated HTML into the ad container
+ad.innerHTML = adHTML;
+
+// Append the ad container to an element with the ID 'ad-container' on the page
+document.getElementById('ad-container').appendChild(ad);
